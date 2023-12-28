37ae1197613c49d61c8d179d1277f23fbf1dae26
Kyung-Wan Woo <wkw8402@berkeley.edu>
2023-12-27 22:44:51 -0800

    first commit

---
+++ fresh/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  let name = 'woo'
+  return (
+    <main>
+      <h4 className='title'>Apple Fresh</h4>
+      <p className='title-sub'>by dev { name }</p>
+       {/* data binding  */}
+    </main>
+  )
+}
+
