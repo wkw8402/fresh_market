0ab8fafaac626ecc8880818e293e1bb97b6076f9
Kyung-Wan Woo <wkw8402@berkeley.edu>
2023-12-30 00:35:04 -0800

    props and state

--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,17 +1,20 @@
 export default function Cart() {
+    let cart = ['Tomatoes', 'Pasta']
     return (
-      <div>
-        <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>item</p>
-          <p>$40</p>
-          <p>1</p>
+        <div>
+            <h4 className="title">Cart</h4>
+            <CartItem item={cart[0]}/>
+            <CartItem item={cart[1]}/>
         </div>
-        <div className="cart-item">
-          <p>item</p>
-          <p>$40</p>
-          <p>1</p>
-        </div>
-      </div>
     )
-  }
+}
+
+function CartItem(props){
+    return(
+        <div className="cart-item">
+            <p>{props.item}</p>
+            <p>$40</p>
+            <p>1</p>
+        </div>
+    )
+}
--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,5 +1,10 @@
+'use client'
+
+import { useState } from "react"
+
 export default function List() {
     let items = ['Tomatoes', 'Pasta', 'Coconut']
+    let [quantity, setQuantity] = useState(0)
 
     return (
       <div>
@@ -10,6 +15,8 @@ export default function List() {
               <div className="food" key={i}>
                 <img src={`/food${i}.png`} className="food-img" />
                 <h4>{item} $40</h4>
+                <span> {quantity} </span>
+                <button onClick={()=>{  setQuantity(quantity+1)  }}> + </button>
               </div>
             )
           })
